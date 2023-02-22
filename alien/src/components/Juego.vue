aaf28a87f0f825a123dac214495dc82a7466dd9f
asier <forpruebasclase@gmail.com>
2023-02-22 11:59:28 +0100

    no message

--- alien/src/components/Juego.vue
+++ alien/src/components/Juego.vue
@@ -1,83 +1,115 @@
 <template>
     <div>
-        <img src="../assets/ufo.gif" id="alien">  
+        <img src="../assets/ufo.gif" id="alien">
     </div>
 </template>
 
 <script>
-    export default {
-        data() {
-            return {
+export default {
+    data() {
+        return {
 
-            }
-        }, 
-        methods: {
-            jump() {
-                console.log(document.getElementById('alien').classList.value);
-
-                if(!document.getElementById('alien').classList.contains('jump'))
-                    document.getElementById('alien').className = 'jump';
-                
-
-                setTimeout(() => {
-                    document.getElementById('alien').className = '';
-                }, 800);
-            },
-            randomObjects() {
-                setTimeout(() => {
-                    let randNum = Math.floor(Math.random() * 2);
-                    if(randNum == 0) {
-                        
-                    } else if(randNum == 1) {
-
-                    } else {
-
-                    }
-                }, 1000);
-            }
-        },
-        mounted() {
-            window.addEventListener('keypress', this.jump);
         }
+    },
+    methods: {
+        jump(e) {
+
+            console.log(e.keyCode);
+            // console.log(document.getElementById('alien').classList.value);
+
+            if (e.keyCode == '38') {
+                if (!document.getElementById('alien').classList.contains('jump'))
+                    //document.getElementById('alien').className = 'jump';
+                    document.getElementById('alien').style.marginBottom = '5px';
+            }else if (e.keyCode == '40'){
+                if (!document.getElementById('alien').classList.contains('down'))
+                    document.getElementById('alien').className = 'down';
+            }
+
+            setTimeout(() => {
+                document.getElementById('alien').className = '';
+            }, 800);
+        },
+        randomObjects() {
+            setTimeout(() => {
+                let randNum = Math.floor(Math.random() * 2);
+                if (randNum == 0) {
+
+                } else if (randNum == 1) {
+
+                } else {
+
+                }
+            }, 1000);
+        }
+    },
+    mounted() {
+        window.addEventListener('keydown', this.jump);
     }
+}
 </script>
 
 <style>
-    img {
-        width: 30%;
+img {
+    width: 30%;
+}
+
+.jump {
+    animation: jump 0.8s;
+    animation-timing-function: cubic-bezier(.41, .54, .4, 1.02);
+}
+
+.down {
+    animation: down 0.8s;
+    animation-timing-function: cubic-bezier(.41, .54, .4, 1.02);
+}
+
+#alien {
+    height: 3cm;
+    width: 3cm;
+    position: relative;
+    top: 6cm;
+    left: 2cm;
+}
+
+
+@keyframes jump {
+    0% {
+        padding-bottom: 0;
     }
 
-    .jump {
-        animation: jump 0.8s;
-        animation-timing-function: cubic-bezier(.41,.54,.4,1.02); 
+    5% {
+        transform: scale(1.1, 0.95);
     }
 
-    #alien{
-        margin-top: 20%;
-        margin-left: 2cm;
-        height: 3cm;
-        width: 3cm;
+    50% {
+        transform: translateY(-200px);
     }
-    
 
-    @keyframes jump {
-        0% {
-            padding-bottom: 0;
-        }
+    95% {}
 
-        5% {
-            transform: scale(1.1, 0.95);
-        }
-
-        50% {
-            transform: translateY(-100px);
-        }
-
-        95% {
-        }
-
-        100% {
-            padding-bottom: 0;
-        }
+    100% {
+        padding-bottom: 0;
     }
+}
+
+@keyframes down {
+    0% {
+        padding-bottom: 0;
+    }
+
+    5% {
+        transform: scale(1.1, 0.95);
+    }
+
+    50% {
+        transform: translateY(+200px);
+    }
+
+    95% {}
+
+    100% {
+        padding-bottom: 0;
+    }
+}
 </style>
