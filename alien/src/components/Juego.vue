5aef2e683cecf06f83a5621e637bd901b1247572
asier <forpruebasclase@gmail.com>
2023-02-23 09:41:37 +0100

    movimiento completo

--- alien/src/components/Juego.vue
+++ alien/src/components/Juego.vue
@@ -8,53 +8,72 @@
 export default {
     data() {
         return {
-            verticalPosition: 230
+            verticalPosition: 230,
+            estado: "quieto",
+            moviendo: true,
         }
     },
+    watch: {
+        // watching top-level property
+        estado(val, oldVal) {
+          this.moverse();
+        }
+    }
+    ,
     methods: {
-        jump(e) {
-
+        changeState(e) {
             if (e.keyCode == '38') {
-                if (this.verticalPosition > 10) {
-                    this.verticalPosition = this.verticalPosition - 10;
-                    console.log(this.verticalPosition);
-                    document.getElementById('alien').style.top = this.verticalPosition + "px";
-                }
+                this.estado = "subir";
             } else if (e.keyCode == '40') {
-                if (this.verticalPosition < 470) {
-                    this.verticalPosition = this.verticalPosition + 10;
-                    console.log(this.verticalPosition);
-                    document.getElementById('alien').style.top = this.verticalPosition + "px";
-                }
+                this.estado = "bajar";
             }
-
-            setTimeout(() => {
-                document.getElementById('alien').className = '';
-            }, 800);
         },
-        randomObjects() {
-                let obstaculo = document.createElement('div');
-
-                let aleat = (Math.floor(Math.random() * 470)) - 100;
-                obstaculo.style.top = aleat + 'px';
-
-                console.log(aleat);
-
-                
-                obstaculo.id = 'obstaculo';
-                document.getElementById('juego').appendChild(obstaculo);
+        moverse() {
+            if (this.estado == "subir") {
+                this.subir();
+            } else if (this.estado == "bajar") {
+                this.bajar();
+            }
+            if (this.estado != "quieto") {
 
                 setTimeout(() => {
-                    obstaculo.remove()
-                }, 2000);
+                    this.moverse();
+                }, 15);
+            }
         },
+        subir() {
+            if (this.verticalPosition > 10) {
+                this.verticalPosition = this.verticalPosition - 10;
+                document.getElementById('alien').style.top = this.verticalPosition + "px";
+            }
+        },
+        bajar(e) {
+            if (this.verticalPosition < 470) {
+                this.verticalPosition = this.verticalPosition + 10;
+                document.getElementById('alien').style.top = this.verticalPosition + "px";
+            }
+            // setTimeout(() => {
+            //     document.getElementById('alien').className = '';
+            // }, 800);
+        },
+        randomObjects() {
+            setTimeout(() => {
+                let randNum = Math.floor(Math.random() * 2);
+                if (randNum == 0) {
+
+                } else if (randNum == 1) {
+
+                } else {
+
+                }
+            }, 1000);
+        }
     },
     mounted() {
-        window.addEventListener('keydown', this.jump);
-        setTimeout(() => {
-            setInterval(this.randomObjects, 3000);
-            //this.randomObjects();
-        }, 1500);
+        window.addEventListener('keydown', (e) => { this.changeState(e)});
+        window.addEventListener('keyup', () => {
+            this.estado = "quieto";
+        });
     }
 }
 </script>
@@ -64,6 +83,16 @@ img {
     width: 30%;
 }
 
+.jump {
+    animation: jump 1s;
+    animation-timing-function: cubic-bezier(.41, .54, .4, 1.02);
+}
+
+.down {
+    animation: down 1s;
+    animation-timing-function: cubic-bezier(.41, .54, .4, 1.02);
+}
+
 #alien {
     height: 3cm;
     width: 3cm;
@@ -71,25 +100,4 @@ img {
     top: 6cm;
     left: 2cm;
 }
-
-#obstaculo {
-    height: 2cm;
-    width: 2cm;
-    position: relative;
-    left: 33cm;
-    background-color: black;
-    overflow-x: hidden;
-
-    animation: moveObstaculo 2s linear;
-}
-
-
-@keyframes moveObstaculo {
-
-    100% {
-        left: -3cm;
-        display: none;
-    }
-}
-
 </style>
