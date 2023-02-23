9f80e5c720010fa78f4ee19851ec4e4e0ddefe59
asier <forpruebasclase@gmail.com>
2023-02-23 13:02:55 +0100

    audios

--- alien/src/components/Juego.vue
+++ alien/src/components/Juego.vue
@@ -1,7 +1,7 @@
 <template>
-  <div>
-    <img src="../assets/ufo.gif" id="alien" />
-  </div>
+    <div>
+        <img src="../assets/ufo.gif" id="alien" />
+    </div>
 </template>
 
 <script>
@@ -16,7 +16,7 @@ export default {
     watch: {
         // watching top-level property
         estado(val, oldVal) {
-          this.moverse();
+            this.moverse(val);
         }
     }
     ,
@@ -29,53 +29,54 @@ export default {
             }
         },
         randomObjects() {
-                let obstaculo = document.createElement('div');
+            let obstaculo = document.createElement('div');
 
-                let aleat = (Math.floor(Math.random() * 470)) - 100;
-                obstaculo.style.top = aleat + 'px';
-                obstaculo.id = 'obstaculo';
-                document.getElementById('juego').appendChild(obstaculo);
+            let aleat = (Math.floor(Math.random() * 470)) - 100;
+            obstaculo.style.top = aleat + 'px';
+            obstaculo.id = 'obstaculo';
+            document.getElementById('juego').appendChild(obstaculo);
 
-                //console.log(aleat);
-                let hit = setInterval(() => {
-                    console.log('Y Obs:' + obstaculo.getBoundingClientRect().y + ' Bottom Obs: ' + obstaculo.getBoundingClientRect().bottom);
-                    console.log('Y Alien:' + document.getElementById('alien').getBoundingClientRect().y + ' Alien Obs: ' + document.getElementById('alien').getBoundingClientRect().bottom);
-                    if(document.getElementById('alien').getBoundingClientRect().y >= obstaculo.getBoundingClientRect().y
-                        && document.getElementById('alien').getBoundingClientRect().y <= obstaculo.getBoundingClientRect().bottom
-                        && document.getElementById('alien').getBoundingClientRect().x >= obstaculo.getBoundingClientRect().x
-                        && document.getElementById('alien').getBoundingClientRect().x <= obstaculo.getBoundingClientRect().right
-                            || document.getElementById('alien').getBoundingClientRect().y >= obstaculo.getBoundingClientRect().y
-                            && document.getElementById('alien').getBoundingClientRect().y <= obstaculo.getBoundingClientRect().bottom
-                            && document.getElementById('alien').getBoundingClientRect().right >= obstaculo.getBoundingClientRect().x
-                            && document.getElementById('alien').getBoundingClientRect().right <= obstaculo.getBoundingClientRect().right
-                                || document.getElementById('alien').getBoundingClientRect().bottom >= obstaculo.getBoundingClientRect().y
-                                && document.getElementById('alien').getBoundingClientRect().bottom <= obstaculo.getBoundingClientRect().bottom
-                                && document.getElementById('alien').getBoundingClientRect().x >= obstaculo.getBoundingClientRect().x
-                                && document.getElementById('alien').getBoundingClientRect().x <= obstaculo.getBoundingClientRect().right
-                                    || document.getElementById('alien').getBoundingClientRect().bottom >= obstaculo.getBoundingClientRect().y
-                                    && document.getElementById('alien').getBoundingClientRect().bottom <= obstaculo.getBoundingClientRect().bottom
-                                    && document.getElementById('alien').getBoundingClientRect().right >= obstaculo.getBoundingClientRect().x
-                                    && document.getElementById('alien').getBoundingClientRect().right <= obstaculo.getBoundingClientRect().right) {
-                            alert('Chocaste');
-                        }
-                    }, 100);
+            //console.log(aleat);
+            let hit = setInterval(() => {
+                console.log('Y Obs:' + obstaculo.getBoundingClientRect().y + ' Bottom Obs: ' + obstaculo.getBoundingClientRect().bottom);
+                console.log('Y Alien:' + document.getElementById('alien').getBoundingClientRect().y + ' Alien Obs: ' + document.getElementById('alien').getBoundingClientRect().bottom);
+                if (document.getElementById('alien').getBoundingClientRect().y >= obstaculo.getBoundingClientRect().y
+                    && document.getElementById('alien').getBoundingClientRect().y <= obstaculo.getBoundingClientRect().bottom
+                    && document.getElementById('alien').getBoundingClientRect().x >= obstaculo.getBoundingClientRect().x
+                    && document.getElementById('alien').getBoundingClientRect().x <= obstaculo.getBoundingClientRect().right
+                    || document.getElementById('alien').getBoundingClientRect().y >= obstaculo.getBoundingClientRect().y
+                    && document.getElementById('alien').getBoundingClientRect().y <= obstaculo.getBoundingClientRect().bottom
+                    && document.getElementById('alien').getBoundingClientRect().right >= obstaculo.getBoundingClientRect().x
+                    && document.getElementById('alien').getBoundingClientRect().right <= obstaculo.getBoundingClientRect().right
+                    || document.getElementById('alien').getBoundingClientRect().bottom >= obstaculo.getBoundingClientRect().y
+                    && document.getElementById('alien').getBoundingClientRect().bottom <= obstaculo.getBoundingClientRect().bottom
+                    && document.getElementById('alien').getBoundingClientRect().x >= obstaculo.getBoundingClientRect().x
+                    && document.getElementById('alien').getBoundingClientRect().x <= obstaculo.getBoundingClientRect().right
+                    || document.getElementById('alien').getBoundingClientRect().bottom >= obstaculo.getBoundingClientRect().y
+                    && document.getElementById('alien').getBoundingClientRect().bottom <= obstaculo.getBoundingClientRect().bottom
+                    && document.getElementById('alien').getBoundingClientRect().right >= obstaculo.getBoundingClientRect().x
+                    && document.getElementById('alien').getBoundingClientRect().right <= obstaculo.getBoundingClientRect().right) {
+                    alert('Chocaste');
+                }
+            }, 100);
 
-                setTimeout(() => {
-                    obstaculo.remove()
-                    clearInterval(hit);
-                }, 2000);
-            },
-        moverse() {
+            setTimeout(() => {
+                obstaculo.remove()
+                clearInterval(hit);
+            }, 2000);
+        },
+        moverse(direccion) {
             if (this.estado == "subir") {
                 this.subir();
             } else if (this.estado == "bajar") {
                 this.bajar();
             }
             if (this.estado != "quieto") {
-
-                setTimeout(() => {
-                    this.moverse();
-                }, 15);
+                if (this.estado == direccion) {
+                    setTimeout(() => {
+                        this.moverse(direccion);
+                    }, 15);
+                }
             }
         },
         subir() {
@@ -95,7 +96,7 @@ export default {
         },
     },
     mounted() {
-        window.addEventListener('keydown', (e) => { this.changeState(e)});
+        window.addEventListener('keydown', (e) => { this.changeState(e) });
         window.addEventListener('keyup', () => {
             this.estado = "quieto";
         });
@@ -109,25 +110,25 @@ export default {
 
 <style>
 img {
-  width: 30%;
+    width: 30%;
 }
 
 .jump {
-  animation: jump 1s;
-  animation-timing-function: cubic-bezier(0.41, 0.54, 0.4, 1.02);
+    animation: jump 1s;
+    animation-timing-function: cubic-bezier(0.41, 0.54, 0.4, 1.02);
 }
 
 .down {
-  animation: down 1s;
-  animation-timing-function: cubic-bezier(0.41, 0.54, 0.4, 1.02);
+    animation: down 1s;
+    animation-timing-function: cubic-bezier(0.41, 0.54, 0.4, 1.02);
 }
 
 #alien {
-  height: 3cm;
-  width: 3cm;
-  position: relative;
-  top: 6cm;
-  left: 2cm;
+    height: 3cm;
+    width: 3cm;
+    position: relative;
+    top: 6cm;
+    left: 2cm;
 }
 
 #obstaculo {
@@ -139,11 +140,11 @@ img {
     overflow-x: hidden;
     animation: moveObstaculo 2s linear;
 }
+
 @keyframes moveObstaculo {
     100% {
         left: -3cm;
         display: none;
     }
 }
-
 </style>
