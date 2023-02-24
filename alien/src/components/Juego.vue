2f98dbbafc8ce4a38588785b9f503fe932e612e6
XabierCarnero00 <xabigol4@gmail.com>
2023-02-24 08:33:42 +0100

    hit empezando

--- alien/src/components/Juego.vue
+++ alien/src/components/Juego.vue
@@ -11,12 +11,22 @@ export default {
             verticalPosition: 230,
             estado: "quieto",
             moviendo: true,
+            aliens: []
+            
         }
     },
     watch: {
         // watching top-level property
         estado(val, oldVal) {
-          this.moverse();
+            this.moverse();
+        },
+
+        aliens(val, oldVal){
+            // si hay this.aliens
+            // val[0] 
+            // alien.left <= ovni righ
+            //     compruebo si se choca
+            //     alien right = alien.lefg + algo
         }
     }
     ,
@@ -30,40 +40,21 @@ export default {
         },
         randomObjects() {
                 let obstaculo = document.createElement('div');
+                let alien = document.getElementById('alien');
 
                 let aleat = (Math.floor(Math.random() * 470)) - 100;
                 obstaculo.style.top = aleat + 'px';
                 obstaculo.id = 'obstaculo';
                 document.getElementById('juego').appendChild(obstaculo);
 
-                //console.log(aleat);
-                let hit = setInterval(() => {
-                    console.log('Y Obs:' + obstaculo.getBoundingClientRect().y + ' Bottom Obs: ' + obstaculo.getBoundingClientRect().bottom);
-                    console.log('Y Alien:' + document.getElementById('alien').getBoundingClientRect().y + ' Alien Obs: ' + document.getElementById('alien').getBoundingClientRect().bottom);
-                    if(document.getElementById('alien').getBoundingClientRect().y >= obstaculo.getBoundingClientRect().y
-                        && document.getElementById('alien').getBoundingClientRect().y <= obstaculo.getBoundingClientRect().bottom
-                        && document.getElementById('alien').getBoundingClientRect().x >= obstaculo.getBoundingClientRect().x
-                        && document.getElementById('alien').getBoundingClientRect().x <= obstaculo.getBoundingClientRect().right
-                            || document.getElementById('alien').getBoundingClientRect().y >= obstaculo.getBoundingClientRect().y
-                            && document.getElementById('alien').getBoundingClientRect().y <= obstaculo.getBoundingClientRect().bottom
-                            && document.getElementById('alien').getBoundingClientRect().right >= obstaculo.getBoundingClientRect().x
-                            && document.getElementById('alien').getBoundingClientRect().right <= obstaculo.getBoundingClientRect().right
-                                || document.getElementById('alien').getBoundingClientRect().bottom >= obstaculo.getBoundingClientRect().y
-                                && document.getElementById('alien').getBoundingClientRect().bottom <= obstaculo.getBoundingClientRect().bottom
-                                && document.getElementById('alien').getBoundingClientRect().x >= obstaculo.getBoundingClientRect().x
-                                && document.getElementById('alien').getBoundingClientRect().x <= obstaculo.getBoundingClientRect().right
-                                    || document.getElementById('alien').getBoundingClientRect().bottom >= obstaculo.getBoundingClientRect().y
-                                    && document.getElementById('alien').getBoundingClientRect().bottom <= obstaculo.getBoundingClientRect().bottom
-                                    && document.getElementById('alien').getBoundingClientRect().right >= obstaculo.getBoundingClientRect().x
-                                    && document.getElementById('alien').getBoundingClientRect().right <= obstaculo.getBoundingClientRect().right) {
-                            alert('Chocaste');
-                        }
-                    }, 100);
-
                 setTimeout(() => {
-                    obstaculo.remove()
+                    obstaculo.remove();
                     clearInterval(hit);
                 }, 2000);
+
+                setTimeout(() => {
+                    this.randomObjects();
+                }, (Math.floor(Math.random() * 2000)) + 1000);
             },
         moverse() {
             if (this.estado == "subir") {
@@ -101,7 +92,7 @@ export default {
         });
 
         setTimeout(() => {
-            setInterval(this.randomObjects, 2500);
+            this.randomObjects();
         }, 2000);
     }
 }
@@ -109,31 +100,31 @@ export default {
 
 <style>
 img {
-  width: 30%;
+    width: 30%;
 }
 
 .jump {
-  animation: jump 1s;
-  animation-timing-function: cubic-bezier(0.41, 0.54, 0.4, 1.02);
+    animation: jump 1s;
+    animation-timing-function: cubic-bezier(0.41, 0.54, 0.4, 1.02);
 }
 
 .down {
-  animation: down 1s;
-  animation-timing-function: cubic-bezier(0.41, 0.54, 0.4, 1.02);
+    animation: down 1s;
+    animation-timing-function: cubic-bezier(0.41, 0.54, 0.4, 1.02);
 }
 
 #alien {
-  height: 3cm;
-  width: 3cm;
-  position: relative;
-  top: 6cm;
-  left: 2cm;
+    height: 3cm;
+    width: 3cm;
+    position: relative;
+    top: 6cm;
+    left: 2cm;
 }
 
 #obstaculo {
     height: 2cm;
     width: 2cm;
-    position: relative;
+    position: absolute;
     left: 33cm;
     background-color: black;
     overflow-x: hidden;
